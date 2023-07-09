ca0c4b186e58454b4f7cefa27a3e4e70f96b5eef
Ihsan Devs <ihsandevs@ihsandevs.com>
2023-07-09 08:47:51 +0700

    fix header

--- errors/404.php
+++ errors/404.php
@@ -1,13 +1,7 @@
-<?php
-
-header( "HTTP/1.0 404 Not Found" );
-?>
-
-<div class="row">
-    <div class="col-md-6">
-        <h1>404 Not Found</h1>
-        <p>Sorry, the page you are looking for could not be found.</p>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>404 Page Not Found</h1>
+        </div>
     </div>
-</div>
-
 </div>
